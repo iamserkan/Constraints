cac1b6a5068168f6965e35dd101bbba9cb5dc08e
iamserkan <yosoyserkan@gmail.com>
2018-05-05 00:15:53 +0300

    Initial Commit

---
+++ Constraints/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Constraints
+//
+//  Created by Serkan.Ozdemir on 05/05/2018.
+//  Copyright © 2018 Serkan.Ozdemir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
